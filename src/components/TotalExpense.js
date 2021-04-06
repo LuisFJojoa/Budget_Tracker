79c9690f629dc5c8134e3b0c223068b909c719ed
ljojoa <luis.jojoa.q@gmail.com>
2021-04-05 22:10:30 -0500

    first commit

---
+++ src/components/TotalExpense.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TotalExpense = () => {
+	return (
+		<div className='alert alert-primary'>
+			<span>Spent so far: £1000</span>
+		</div>
+	);
+};
+
+export default TotalExpense;
